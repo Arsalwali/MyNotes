bde8667bfee31aecb4ec67dc3fd71a3cf6011292
Arsal Wali <arsalwali@hotmail.com>
2019-01-15 01:35:11 +0500

    Adding firebase mock file

--- __mock__/react-native-firebase.js
+++ __mock__/react-native-firebase.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const collection = jest.fn(() => {
   return {
     doc: jest.fn(() => {
@@ -25,4 +27,58 @@ const Firestore = () => {
 
 Firestore.FieldValue = {
   serverTimestamp: jest.fn()
+}
+
+export default class RNFirebase {
+
+  static initializeApp = jest.fn();
+
+  static auth = jest.fn(() => {
+    return {
+      createUserAndRetrieveDataWithEmailAndPassword: jest.fn(() => Promise.resolve(true)),
+      sendPasswordResetEmail: jest.fn(() => Promise.resolve(true)),
+      signInAndRetrieveDataWithEmailAndPassword: jest.fn(() => Promise.resolve(true)),
+      fetchSignInMethodsForEmail: jest.fn(() => Promise.resolve(true)),
+      signOut: jest.fn(() => Promise.resolve(true)),
+      onAuthStateChanged: jest.fn(),
+      currentUser: {
+        sendEmailVerification: jest.fn(() => Promise.resolve(true))
+      }
+    }
+  });
+
+  static firestore = Firestore;
+
+  static notifications = jest.fn(() => {
+    return {
+        onNotification: jest.fn(),
+        onNotificationDisplayed: jest.fn(),
+        onNotificationOpened: jest.fn()
+    }
+  });
+
+  static messaging = jest.fn(() => {
+    return {
+        hasPermission: jest.fn(() => Promise.resolve(true)),
+        subscribeToTopic: jest.fn(),
+        unsubscribeFromTopic: jest.fn(),
+        requestPermission: jest.fn(() => Promise.resolve(true)),
+        getToken: jest.fn(() => Promise.resolve('RN-Firebase-Token'))
+    }
+  });
+
+  static storage = jest.fn(() => {
+    return {
+      ref: jest.fn(() => {
+        return {
+          child: jest.fn(() => {
+            return {
+              put: jest.fn(() => Promise.resolve(true))
+            }
+          })
+        }
+      })
+    }
+  })
+
 }
